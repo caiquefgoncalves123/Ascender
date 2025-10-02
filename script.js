d8ea0394255c509e613e8b8ab62f85e545077fde
anagregolin <analaura.gregolin09@gmail.com>
2025-10-02 17:23:44 -0300

    CSS responsivo feito, faltando poucas alterações
    Fizemos o CSS responsivo do site inteiro, de todas as telas. Está faltando apenas algumas alterações nas tabelas.

---
+++ script.js
@@ -0,0 +1,28 @@
+function excluir(event) {
+    event.preventDefault(); // impede a função padrão do elemento (no caso um link que iria recarregar a página)
+
+    if (confirm("Tem certeza que deseja excluir esta inscrição?")) {
+        document.getElementById("caixaa").style.display = "none";
+        alert("Inscrição excluída com sucesso!");
+    } else {
+        alert("Exclusão cancelada.");
+    }
+}
+
+function esgotouAula(event) {
+
+    event.preventDefault()
+
+      document.getElementById("meuModal").style.display = "flex";
+    }
+
+    function fecharModal() {
+      document.getElementById("meuModal").style.display = "none";
+    }
+
+  
+
+
+    
+
+
